e39c2b1351ddbb6d49bf5a6ca2cbdcdad10a14c0
Dmitry <75439805+alexotl777@users.noreply.github.com>
2024-04-23 15:11:11 +0300

    Delete authentication directory

--- authentication/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class authentication(models.Model):
-    phone = models.CharField(max_length=255)
-    invite = models.CharField(max_length=255)
-    def __str__(self):
-        return self.phone
